0ad440716c0d77003639f8ddd82400fde1fca50a
Sumit <kumar1993sumit15@gmail.com>
2018-01-03 01:48:54 +0530

    Added append-to-tasktable, dispatcher

--- src/todo_frontend/handler.cljs
+++ src/todo_frontend/handler.cljs
@@ -4,11 +4,42 @@
   (:require [todo-frontend.apicalls :as apis]
             [todo-frontend.templates :as tpl]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]]
+            [goog.events :as events]
+            [goog.dom :as dom]
+            [goog.history.EventType :as EventType]))
+
+(defn set-html! [el content]
+  (set! (.-innerHTML el) content))
 
 (defn load-tasks
   [username]
   (go
     (let [resp (<! (apis/get-user-tasks username))]
       (html
-        [:div (apply str (map (fn [x] (tpl/tpl-task x)) resp))]))))
+        [:table [:tbody {:id "tasktable"} (apply str (map (fn [x] (tpl/tpl-task x)) resp))]]))))
+
+(defn append-to-tasktable
+  [^:Map task-map]
+  (let [parent (dom/getElement "tasktable")
+        childstr (tpl/tpl-task task-map)
+        c-elem (let [d (dom/createElement "tr")]
+                 (-> d (.setAttribute "id" (:_id task-map)))
+                 (set-html! d childstr)
+                 d)
+        ]
+    (dom/appendChild  parent c-elem)))
+
+(defmulti dispatcher
+          "Polymorphic function that handles the response based
+          on :action of response"
+          (fn [param tasks-state] (:action param :default)))
+
+(defmethod dispatcher "newTask"
+  [^:Map resp ^:atom tasks-state]
+  (if (= (:status resp) "success")
+    (append-to-tasktable (:data resp))
+    nil))
+
+(defmethod dispatcher :default
+  [])
