a03dc9d96ff1fe6e17e0b02069f67e4884278329
Sumit <kumar1993sumit15@gmail.com>
2018-01-04 14:45:12 +0530

    changed template to set checkbox state

--- src/todo_frontend/templates.cljs
+++ src/todo_frontend/templates.cljs
@@ -1,6 +1,4 @@
 (ns todo-frontend.templates
-  ;(:use-macros [crate.def-macros :only [defpartial]])
-  ;(:require [crate.core :as crate])
   (:require-macros [hiccups.core :as hiccups :refer [html]])
   (:require [hiccups.runtime :as hiccupsrt]))
 
@@ -14,12 +12,21 @@
     [:div {:class "notfound"}
      "Page not found"]))
 
+(defn check?
+  [s]
+  (if (= s "C")
+    true
+    false))
+
 (defn tpl-task
   [^:Map task-map]
   (html
     [:tr {:id (:_id task-map) :class "row-elem"}
-     [:td [:input {:type "checkbox" :class "chk-box"}]]
-     [:td [:h3 {:class "title-btn"} (:title task-map)]]
+     [:td [:input {:type "checkbox"
+                   :class "chk-box"
+                   :value (if (check? (:status task-map)) "on" "off")
+                   :checked (check? (:status task-map))}]]
+     [:td [:h3 (:title task-map)]]
      [:td [:p (:desc task-map)]]
      [:td [:button {:type "button" :value (:_id task-map) :class "delete-btn"} "Delete"]]]
     ;[:div {:id (:_id task-map) :class "task-layer"}]
