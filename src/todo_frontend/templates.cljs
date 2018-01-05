3e5e7be1f42a9dfbef9ce54d7c8f48df43a04c55
Sumit <kumar1993sumit15@gmail.com>
2018-01-05 09:37:18 +0530

    Delete button redesigned

--- src/todo_frontend/templates.cljs
+++ src/todo_frontend/templates.cljs
@@ -28,6 +28,7 @@
                    :checked (check? (:status task-map))}]]
      [:td [:h3 (:title task-map)]]
      [:td [:p (:desc task-map)]]
-     [:td [:button {:type "button" :value (:_id task-map) :class "delete-btn"} "Delete"]]]
+     [:td [:button {:type "button" :value (:_id task-map) :class "delete-btn"}
+           [:i {:class "material-icons"} "delete"]]]]
     ;[:div {:id (:_id task-map) :class "task-layer"}]
     ))
